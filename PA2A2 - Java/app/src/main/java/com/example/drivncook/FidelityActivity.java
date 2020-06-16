f50fe55b515be8e96eea26b03e28411b3c844447
Célia <celia.allaoua@outlook.fr>
2020-06-16 16:20:28 +0200

    fidelity Test

--- PA2A2 - Java/app/src/main/java/com/example/drivncook/FidelityActivity.java
+++ PA2A2 - Java/app/src/main/java/com/example/drivncook/FidelityActivity.java
@@ -2,8 +2,16 @@ package com.example.drivncook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 public class FidelityActivity extends AppCompatActivity {
 
@@ -13,9 +21,18 @@ public class FidelityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fidelity);
+        final User user = (User) Objects.requireNonNull(getIntent().getExtras()).getSerializable("user");
+
+        AsyncTask<String, Void, String> advantage = new ApiAdvantage(user);
+        try {
+            JSONObject json = new JSONObject(advantage.execute().get());
+            Log.d("allaoua", "onCreate: "+json);
+        } catch (JSONException | ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
 
         listView = findViewById(R.id.allFidelity);
-        FidelityAdapter adapter = new FidelityAdapter(MainActivity.this, getData());
+        //FidelityAdapter adapter = new FidelityAdapter(FidelityActivity.this, getData());
 
     }
 
--- PA2A2 - Java/app/src/main/java/com/example/drivncook/FidelityAdapter.java
+++ PA2A2 - Java/app/src/main/java/com/example/drivncook/FidelityAdapter.java
@@ -1,4 +1,56 @@
 package com.example.drivncook;
 
-public class FidelityAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FidelityAdapter extends BaseAdapter {
+
+    private List<Fidelity> fidelityList;
+    private Context context;
+
+    public FidelityAdapter(Context context, List<Fidelity> fidelityList){
+        this.context = context;
+        this.fidelityList = fidelityList;
+    }
+
+
+    @Override
+    public int getCount() {
+        return fidelityList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.fidelityList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+       if (convertView == null){
+           LayoutInflater inflater = LayoutInflater.from(this.context);
+           convertView = inflater.inflate(R.layout.row,null);
+       }
+       TextView title = convertView.findViewById(R.id.title);
+       TextView points = convertView.findViewById(R.id.points);
+       TextView category = convertView.findViewById(R.id.category);
+
+       Advantage advantage = (Advantage) getItem(position);
+
+       title.setText(advantage.getAdvantageName());
+       category.setText(advantage.getCategoryName());
+       points.setText(advantage.getNbPoint());
+
+       return convertView;
+    }
 }

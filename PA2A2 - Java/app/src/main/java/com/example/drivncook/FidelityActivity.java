640254f315e8cb4bbaaf5c717502d46036b9ac76
Célia <celia.allaoua@outlook.fr>
2020-06-16 12:36:56 +0200

    add FidelityAdapter.java

--- PA2A2 - Java/app/src/main/java/com/example/drivncook/FidelityActivity.java
+++ PA2A2 - Java/app/src/main/java/com/example/drivncook/FidelityActivity.java
@@ -3,12 +3,22 @@ package com.example.drivncook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class FidelityActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fidelity);
+
+        listView = findViewById(R.id.allFidelity);
+        FidelityAdapter adapter = new FidelityAdapter(MainActivity.this, getData());
+
     }
+
+
+
 }
---
+++ PA2A2 - Java/app/src/main/java/com/example/drivncook/FidelityAdapter.java
@@ -0,0 +1,4 @@
+package com.example.drivncook;
+
+public class FidelityAdapter {
+}
